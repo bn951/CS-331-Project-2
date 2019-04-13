78b0cd9bda0e74a85b0b152585425778cb59430c
Bobby Nijjar <bobby951@gmail.com>
2019-04-13 14:06:27 -0400

    [Bobby] added docs

--- Project2_G9-5.sql
+++ Project2_G9-5.sql
@@ -1,22 +1,24 @@
 USE BIClass
 GO
 
+-- ============================================= 
+-- Author:  Bobby Nijjar
+-- Procedure:   DbSecurity.UserAuthorization
+-- Create date:  4/12
+-- Description: Table containing each Group Member
+
+-- ==============================================
+
 -- Create Schemas
-DROP SCHEMA IF EXISTS DbSecurity;
-GO
 CREATE SCHEMA DbSecurity;
-GO
-DROP SCHEMA IF EXISTS PkSequence;
 GO
 CREATE SCHEMA PkSequence;
 GO
 
 -- Create Sequence Object for UserAuthorization Table
-DROP SEQUENCE IF EXISTS PkSequence.UserAuthorizationSequenceObject;
 CREATE SEQUENCE PkSequence.UserAuthorizationSequenceObject AS INT MINVALUE 1;
 
 -- Create UserAuthorization Table
-DROP TABLE IF EXISTS DbSecurity.UserAuthorization
 CREATE TABLE DbSecurity.UserAuthorization(
     UserAuthorizationKey INT            NOT NULL    DEFAULT (NEXT VALUE FOR PkSequence.UserAuthorizationSequenceObject) PRIMARY KEY,
     ClassTime            NCHAR(5)       NULL        DEFAULT '9:15',
